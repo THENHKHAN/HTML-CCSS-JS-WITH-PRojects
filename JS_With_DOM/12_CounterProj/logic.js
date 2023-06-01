9d4e58c28e6c41b3362eed7c2df651d6a0ef1696
Noorul Huda Khan <noorulhuda101099@gmail.com>
2023-06-02 01:02:26 +0530

    Counter APP completed with 3 functionalities

--- JS_With_DOM/12_CounterProj/logic.js
+++ JS_With_DOM/12_CounterProj/logic.js
@@ -1,2 +1,56 @@
 
 console.log("JS Loaded!")
+
+let incr = document.querySelector(".incr");
+let decr = document.querySelector(".decr");
+let reset = document.querySelector(".reset");
+let output = document.querySelector("#result");
+
+
+// lets check whether we got element or not using console:
+
+console.log(`Incre: ${incr.innerHTML}`);
+console.log(`decr: ${decr.innerHTML}`);
+console.log(`reset: ${reset.innerHTML}`);
+console.log(`output: ${output.innerHTML}`);
+
+// now lets write logic
+// there are multiple method but i am using intermediate logic.
+let allBtns = document.querySelector("#allBtns") // get list of element
+console.log(   `allBtns : ${allBtns.innerHTML}`)
+
+//  now we'll get the specific btn using event handler i.e. target then we'll compare
+
+allBtns.addEventListener('click',counter)
+
+let count = 0
+function counter (e) {
+    let ele = e.target // getting element
+    console.log(`element: ${ele} `)// clicked btn
+    console.log(`content: ${ele.innerHTML} `)// content
+    console.log(`Pressed btn: ${ele.className} `) //class name
+
+    // now we'll compare according to clicked btn
+
+    if (ele.className==="decr style"){
+
+        count -= 1;
+        console.log(`counter: ${count}`)
+        
+    }
+
+    
+    else if (ele.className==="reset"){
+         count = 0
+         console.log(`counter: ${count}`)
+    }
+   
+else{
+        count += 1
+         console.log(`counter: ${count}`)
+}
+ output.innerHTML = count
+
+   
+
+}
