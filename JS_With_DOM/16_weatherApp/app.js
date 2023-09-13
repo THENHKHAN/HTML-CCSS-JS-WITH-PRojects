63c6102d7d8d25c053a30d02073b29750b90f767
Noorul Huda Khan <noorulhuda101099@gmail.com>
2023-09-13 07:39:22 +0530

    Waether App Project Added

---
+++ JS_With_DOM/16_weatherApp/app.js
@@ -0,0 +1,97 @@
+
+const searchBtn = document.querySelector('button')
+const inputText = document.querySelector('#input_text')
+const temp = document.querySelector('.temp')
+const humidity = document.querySelector('.humidity')
+const wind = document.querySelector('.wind')
+const weatherCard = document.querySelector('.weather')
+
+
+
+const apiKey="ec7754f16bcd129dfbbcfbab90ed23a1"
+const apiUrl = `https://api.openweathermap.org/data/2.5/weather?&units=metric&q=`
+
+// let exApi =`https://api.openweathermap.org/data/2.5/weather?q=bengaluru&appid=${apiKey}&units=metric`
+
+
+
+const weather = (weatherType)=>{
+    const weatherIcon = document.querySelector('.weather-icon')
+   
+    if (weatherType.toLowerCase()=="clouds"){
+       weatherIcon.src = './images/clouds.png'
+       console.log("weatherType-> " + weatherType)
+    }
+
+    else if (weatherType.toLowerCase()=="clear"){
+        weatherIcon.src = './images/clear.png'
+        console.log("weatherType-> " + weatherType)
+     }
+
+     else if (weatherType.toLowerCase()=="drizzle"){
+        weatherIcon.src = './images/drizzle.png'
+        console.log("weatherType-> " + weatherType)
+     }
+
+     else if (weatherType.toLowerCase()=="mist"){
+        weatherIcon.src = './images/mist.png'
+        console.log("weatherType-> " + weatherType)
+     }
+
+     else if (weatherType.toLowerCase()=="snow"){
+        weatherIcon.src = './images/snow.png'
+        console.log("weatherType-> " + weatherType)
+     }
+
+     else if (weatherType.toLowerCase()=="rain"){
+        weatherIcon.src = './images/rain.png'
+        console.log("weatherType-> " + weatherType)
+     }
+
+     else{
+        weatherIcon.innerHTML = "Sorry don't have image"
+        console.log("weatherType-> " + weatherType)
+     }
+
+
+}
+
+   
+async function checkWeather (inputCity){
+
+    let raw = await fetch(apiUrl+`${inputCity}`+`&appid=${apiKey}`)
+     let data = await raw.json()
+    console.log(raw)
+    console.log("Temp: "+data.main.temp)
+    console.log("humidity: "+data.main.humidity)
+    console.log("windSpeed: "+data.wind.speed)
+    console.log("city: "+data.name) // city name
+    console.log("weather type: "+data.weather[0].main) // city name
+
+    document.querySelector('.temp').innerHTML=Math.round(data.main.temp) +" °C"
+    document.querySelector('.city').innerHTML=data.name // get the city name and display
+    
+    document.querySelector('.humidity').innerHTML=data.main.humidity+"%"
+    document.querySelector('.wind').innerHTML=data.wind.speed +" km/h"
+    weather(data.weather[0].main) // sending weather type to apply the weather type image
+  
+    weatherCard.style.display='block';// weather card will display detail when someone enter the city name and search button
+
+   }
+
+
+let takeInputText = ()=>{
+
+   if (inputText.value ===''){ // it will only work if there is some input data
+      alert("Please enter any city name!")
+   }
+else{
+
+   const inputCity = inputText.value
+   checkWeather(inputCity)
+  
+}   
+
+}
+
+searchBtn.addEventListener('click' , takeInputText)// to get the entered input data
---
+++ JS_With_DOM/16_weatherApp/readMe.md
@@ -0,0 +1,10 @@
+
+## if city is not entered  (Starter page):
+![Alt text](image.png)
+
+
+## if city is entered :
+![Alt text](image-1.png)
+
+## if user try to search without entering the city name:
+![Alt text](image-2.png)
